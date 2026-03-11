20db8a4d037fae0a45294d5b3923d8ded50908e4
BlackHat-Magic <personal@lukeh.us>
2026-03-11 12:39:39 -0700

    feat/flexbox: prototype for getting node line size

--- slayer_ui/layout.py
+++ slayer_ui/layout.py
@@ -3,150 +3,150 @@ from enum import auto, Enum
 from typing import Any, Callable
 
 class ContentType(Enum):
-    """
-    Type of content inside of a node
-    e.g., image, text, etc.
-    """
+	"""
+	Type of content inside of a node
+	e.g., image, text, etc.
+	"""
 
-    TEXT = auto()
-    IMAGE = auto()
-    SVG = auto()
+	TEXT = auto()
+	IMAGE = auto()
+	SVG = auto()
 
 # ===== MEASUREMENT =====
 
 class Unit(Enum):
-    """
-    Choose what unit a measurement will be
-    """
+	"""
+	Choose what unit a measurement will be
+	"""
 
-    PX = auto()
-    VW = auto()
-    VH = auto()
-    PERCENT = auto()
+	PX = auto()
+	VW = auto()
+	VH = auto()
+	PERCENT = auto()
 
 @dataclass
 class Measurement:
-    """
-    How wide/tall/etc something is
-    """
+	"""
+	How wide/tall/etc something is
+	"""
 
-    unit: Unit
-    value: float
+	unit: Unit
+	value: float
 
 # ===== DISPLAY =====
 
 class Display(Enum):
-    """
-    Display: flex, grid, block, etc
-    """
+	"""
+	Display: flex, grid, block, etc
+	"""
 
-    FLEX = auto()
-    GRID = auto()
+	FLEX = auto()
+	GRID = auto()
 
 # ===== FLEX =====
 
 class Direction(Enum):
-    """
-    Flex direction
-    """
+	"""
+	Flex direction
+	"""
 
-    ROW = auto()
-    ROW_REVERSE = auto()
-    COLUMN = auto()
-    COLUMN_REVERSE = auto()
+	ROW = auto()
+	ROW_REVERSE = auto()
+	COLUMN = auto()
+	COLUMN_REVERSE = auto()
 
 class JustifyContent(Enum):
-    """
-    Justify content
-    """
+	"""
+	Justify content
+	"""
 
-    START = auto()		# flex-start
-    END = auto()		# flex-end
-    CENTER = auto()
-    BETWEEN = auto()	# space-between
-    AROUND = auto()		# space-around
-    EVENLY = auto()		# space-evenly
+	START = auto()		# flex-start
+	END = auto()		# flex-end
+	CENTER = auto()
+	BETWEEN = auto()	# space-between
+	AROUND = auto()		# space-around
+	EVENLY = auto()		# space-evenly
 
 class Align(Enum):
-    """
-    Align items
-    """
+	"""
+	Align items
+	"""
 
-    AUTO = auto()
-    START = auto()	# flex-start
-    END = auto()	# flex-end
-    CENTER = auto()
-    BASELINE = auto()
-    STRETCH = auto()
+	AUTO = auto()
+	START = auto()	# flex-start
+	END = auto()	# flex-end
+	CENTER = auto()
+	BASELINE = auto()
+	STRETCH = auto()
 
 class Wrap(Enum):
-    """
-    Flex Wrap
-    """
+	"""
+	Flex Wrap
+	"""
 
-    NOWRAP = auto()
-    WRAP = auto()
-    REVERSE = auto()
+	NOWRAP = auto()
+	WRAP = auto()
+	REVERSE = auto()
 
 @dataclass
 class Style:
-    """
-    Flex style
-    """
+	"""
+	Flex style
+	"""
 
-    grow: float = 0.0
-    shrink: float = 1.0
-    basis: float | None = None
+	grow: float = 0.0
+	shrink: float = 1.0
+	basis: float | None = None
 
-    direction: Direction = Direction.ROW
-    wrap: Wrap = Wrap.NOWRAP
+	direction: Direction = Direction.ROW
+	wrap: Wrap = Wrap.NOWRAP
 
-    justify_content: JustifyContent = JustifyContent.START
-    align: Align = Align.STRETCH
-    align_self: Align | None = None
+	justify_content: JustifyContent = JustifyContent.START
+	align: Align = Align.STRETCH
+	align_self: Align | None = None
 
-    margin: list[Measurement] = [
+	margin: list[Measurement] = [
 		Measurement(Unit.PX, 0.0),	# top
 		Measurement(Unit.PX, 0.0),	# right
 		Measurement(Unit.PX, 0.0),	# bottom
 		Measurement(Unit.PX, 0.0),	# left
-    ]
-    border: list[Measurement] = [
+	]
+	border: list[Measurement] = [
 		Measurement(Unit.PX, 0.0),	# top
 		Measurement(Unit.PX, 0.0),	# right
 		Measurement(Unit.PX, 0.0),	# bottom
 		Measurement(Unit.PX, 0.0),	# left
-    ]
-    padding: list[Measurement] = [
+	]
+	padding: list[Measurement] = [
 		Measurement(Unit.PX, 0.0),	# top
 		Measurement(Unit.PX, 0.0),	# right
 		Measurement(Unit.PX, 0.0),	# bottom
 		Measurement(Unit.PX, 0.0),	# left
-    ]
-    row_gap: Measurement = Measurement(Unit.PX, 0.0)
-    column_gap: Measurement = Measurement(Unit.PX, 0.0)
+	]
+	row_gap: Measurement = Measurement(Unit.PX, 0.0)
+	column_gap: Measurement = Measurement(Unit.PX, 0.0)
 
-    background_color: list[float] = [1.0, 1.0, 1.0, 1.0]	# rgba
-    border_color: list[float] = [0.0, 0.0, 0.0, 0.0]		# rgba
-    color: list[float] = [0.0, 0.0, 0.0, 1.0]				# rgba
+	background_color: list[float] = [1.0, 1.0, 1.0, 1.0]	# rgba
+	border_color: list[float] = [0.0, 0.0, 0.0, 0.0]		# rgba
+	color: list[float] = [0.0, 0.0, 0.0, 1.0]				# rgba
 
-    width: Measurement | None = None
-    min_width: Measurement | None = None
-    max_width: Measurement | None = None
-    height: Measurement | None = None
-    min_height: Measurement | None = None
-    max_height: Measurement | None = None
+	width: Measurement | None = None
+	min_width: Measurement | None = None
+	max_width: Measurement | None = None
+	height: Measurement | None = None
+	min_height: Measurement | None = None
+	max_height: Measurement | None = None
 
-    top: Measurement = Measurement(Unit.PX, 0.0)
-    left: Measurement = Measurement(Unit.PX, 0.0)
+	top: Measurement = Measurement(Unit.PX, 0.0)
+	left: Measurement = Measurement(Unit.PX, 0.0)
 
 class Node:
-    style: Style = Style()
-    parent: Node | None = None
-    children: list[Node] = []
-    content: Any
+	style: Style = Style()
+	parent: Node | None = None
+	children: list[Node] = []
+	content: Any
 
-    computed: dict = {
+	computed: dict = {
 		"border_box": {
 			"x": None,
 			"y": None,
@@ -165,70 +165,93 @@ class Node:
 			"w": None,
 			"h": None,
 		},
-    }
+	}
 
-    def isLeaf(self) -> bool:
-        return len(self.children) == 0
+	def isLeaf(self) -> bool:
+		return len(self.children) == 0
 
 def getNodeLineSize(
 	node: Node,
 	direction: Direction,
-	getStringWidth: Callable[[str], float],
-	getImageWidth: Callable[[Image], float],
-	getSVGWidth: Callable[[str], float],
+	getStringSize: Callable[[str, Direction], float],
+	getImageSize: Callable[[Image, Direction], float],
+	getSVGSize: Callable[[str, Direction], float],
 ) -> float:
-    """
-    Gets the size of a node in a line
-    	width for nodes in flex-direction=row or flex-direction=row-reverse
-    	height for nodes in flex-direction=column or flex-direction=column-reverse
-    """
+	"""
+	Gets the size of a node in a line
+		width for nodes in flex-direction=row or flex-direction=row-reverse
+		height for nodes in flex-direction=column or flex-direction=column-reverse
+	"""
+	content_size = 0
+	if node.isLeaf():
+		if node.content_type == ContentType.STRING:
+			content_size = getStringSize(node.content, direction)
+		elif node.content_type == ContentType.IMAGE:
+			content_size = getImageSize(node.content, direction)
+		elif node.content_type == ContentType.SVG:
+			content_size = getSVGSize(node.content, direction)
+		else:
+		   	raise NotImplementedError("Only getters for string, image, svg width are implemented")
+		if direction == Direction.ROW or direction == Direction.ROW_REVERSE:
+			return node.style.border[1] + node.style.border[3] + \
+				node.style.border[1] + node.style.border[3] + \
+				content_size
+		elif direction == Direction.COLUMN or direction == Direction.COLUMN_REVERSE:
+			return node.style.border[0] + node.style.border[2] + \
+				node.style.border[0] + node.style.border[2] + \
+				content_size
 
-	if direction == Direction.ROW or direction == Direction.ROW_REVERSE:
-	    if node.isLeaf():
-	        content_width = 0
-	        if node.content_type == ContentType.STRING:
-	            content_width = getStringWidth(node.content)
-	        elif node.content_type == ContentType.IMAGE:
-	            content_width = getImageWidth(node.content)
-	        elif node.content_type == ContentType.SVG:
-	            content_width = getSVGWidth(node.content)
-	       	else:
-	           	raise NotImplementedError("Only getters for string, image, svg width are implemented")
-
-	        return node.style.border[1] + node.style.border[3] + \
-	        	node.style.border[0] + node.style.border[0] + \
-	        	content_width
-	    if node.style.direction == Direction.ROW or node.style.direction == Direction.ROW_REVERSE:
-    	    
+	row_styles = [Direction.ROW, Direction.ROW_REVERSE]
+	column_styles = [Direction.COLUMN, Direction.COLUMN_REVERSE]
+	if (direction in row_styles and node.style.direction in row_styles) \
+		or (direction in column_styles and node.style.direction in column_styles):
+		return sum([getNodeLineSize(
+			child,
+			direction,
+			getStringSize,
+			getImageSize,
+			getSVGSize
+		) for child in node.children])
+	elif (direction in row_styles and node.style.direction in column_styles) \
+		or (direction in column_styles and node.style.direction in row_styles):
+		return max([getNodeLineSize(
+			child,
+			direction,
+			getStringSize,
+			getImageSize,
+			getSVGSize
+		) for child in node.children])
+	else:
+		raise ValueError("Invalid flex-direction")
 
 def layoutNode(node: Node, parent_width: float, parent_height: float) -> None:
-    """
-    Compute the layout for a node
-    """
+	"""
+	Compute the layout for a node
+	"""
 
 	# determine available space (border-box by default)
-    inner_width: float = parent_width - \
-    	node.style.border[1] - node.style.border[3] - \
-    	node.style.padding[1] - node.style.padding[3]
-    inner_height: float = parent_height - \
-    	node.style.border[0] - node.style.border[2] - \
-    	node.style.padding[0] - node.style.padding[2]
+	inner_width: float = parent_width - \
+		node.style.border[1] - node.style.border[3] - \
+		node.style.padding[1] - node.style.padding[3]
+	inner_height: float = parent_height - \
+		node.style.border[0] - node.style.border[2] - \
+		node.style.padding[0] - node.style.padding[2]
 
-    # TODO: split children into lines
+	# TODO: split children into lines
 
-    # process each line (???)
-    for line in lines:
-        # TODO: resolve main axis sizes (grow/shrink)
-        # TODO: align with main axis (justify content)
-        # TODO: align cross axis (align items)
-        pass
+	# process each line (???)
+	for line in lines:
+		# TODO: resolve main axis sizes (grow/shrink)
+		# TODO: align with main axis (justify content)
+		# TODO: align cross axis (align items)
+		pass
 
-    if len(lines) > 1:
-        # TODO: align content if multiple lines exist
-        pass
+	if len(lines) > 1:
+		# TODO: align content if multiple lines exist
+		pass
 
-    for child in node.children:
-        # child.x and child.y are now set (???)
-        # child.width and child.height are now set (???)
-        layoutNode(child, child.width, child.height)
+	for child in node.children:
+		# child.x and child.y are now set (???)
+		# child.width and child.height are now set (???)
+		layoutNode(child, child.width, child.height)
 
